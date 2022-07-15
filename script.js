41e8bf70da73232174e89de25cba89b52332bc49
Malka Anjum <75417185+MALKA-ANJUM@users.noreply.github.com>
2022-07-15 22:55:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// cards
+
+const sections = document.querySelectorAll('.section')
+  
+
+sections.forEach((section)=>{
+section.addEventListener('click',()=>{
+    // remove active class from all another section 
+    // and add it to the selected section
+    sections.forEach((section) => {
+        section.classList.remove('active')
+    })
+    section.classList.add('active')
+})
+})
